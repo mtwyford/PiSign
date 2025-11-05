3da81d282b66d743d688d33d254607d3d562fadd
Marcus T <mtwyford@users.noreply.github.com>
2025-11-05 10:33:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ async function fetchCSV() {
     const sheetUrl = ' https://docs.google.com/spreadsheets/d/1gPvAvWVj5YYGjr8Anrexaj09sb-HjuFb/pub?gid=1432611856&single=true&output=csv'; //twyford
     //const imageUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSzVOY3ul6m0PHSS3TT9WjVyZ3-qlIUcZXe-13xkJsyUKYRl8VVIAzyS_gX26a5bVy7zM2cQ3OqLory/pub?gid=2119881206&single=true&output=csv'; //zeng
     //const imageUrl = 'https://docs.google.com/spreadsheets/d/1gPvAvWVj5YYGjr8Anrexaj09sb-HjuFb/pub?gid=1889703556&amp;single=true&output=csv';  //twyford
-    const imageUrl = 'https://docs.google.com/spreadsheets/d/1v8CvRDsi0bInHhPlR-wjkgOA27JV-CMQ/pub?gid=1889703556&amp;single=true&output=csv'; //science by twy
+    //to make a publicly available sheet, file, publish to web, each sheet as csv
+    const imageUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTU3WrVa8XKB8AJA1VTH1zwBXQbVBkDwZaeTr_VQIj5CUIJ7QhPiU3-TVwjlYjjrA/pub?gid=1889703556&single=true&output=csv'; //science by twy
     try {
         var response = await fetch(sheetUrl);
         var response1 = await fetch(imageUrl);
